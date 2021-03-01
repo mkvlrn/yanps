b82780be92db04be7a877a29bc9e567237be2294
Mike Valeriano <mkvlrn@gmail.com>
2021-02-28 22:00:07 -0300

    Fix package.json template

--- src/index.ts
+++ src/index.ts
@@ -1,6 +1,4 @@
 import { join, resolve } from "path";
-import { exec } from "child_process";
-import { promisify } from "util";
 import ora from "ora";
 
 import prompt from "./prompt";
@@ -8,8 +6,6 @@ import createStructure from "./createStructure";
 import getFile from "./getFile";
 
 (async () => {
-  const doExec = promisify(exec);
-
   ora("yanps").warn();
   const {
     projectPath,
@@ -20,7 +16,11 @@ import getFile from "./getFile";
 
   const projectInit = ora("Initializing project").start();
   await createStructure(projectPath, [".vscode", "src", "tests"]);
-  await doExec(`${projectManager} init -y`, { cwd: resolve(projectPath) });
+  const packFile = `project/${projectLang}-${
+    projectReact ? "react" : "base"
+  }.package.json`;
+  console.log(packFile);
+  await getFile(packFile, join(projectPath, "package.json"));
   projectInit.succeed("Project initialized");
 
   await getFile("git/.gitattributes", join(projectPath, ".gitattributes"));
